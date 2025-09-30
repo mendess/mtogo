785691f340fbb490b5685c441d631723211e3c92
mendess <pedro.mendes.26@gmail.com>
2025-09-30 19:00:39 +0100

    Release 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.mendess.mtogo"
         minSdk = 29
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
